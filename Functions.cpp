b249217bff5ddaeeca97383123b8a38687e90b2b
Ryan Eurich <reurich@uci.edu>
2022-09-23 23:03:39 -0700

    need to add spaces

--- Functions.cpp
+++ Functions.cpp
@@ -2,10 +2,35 @@
 
 string createPyramid(int num)
 {
-
+    if (num = 0){
+        return "Invalid pyrimad size";
+    }
+    else if (num < 0){
+        return "Negative pyrimad size";
+    }
+    
+    std:string str;
+    int iter = 1;
+    for (int i = 0; i < num; num++){
+        int j = 0;
+        while (j < iter){
+            str = str + "-";
+            j++;
+        }
+        iter +=2;
+        int k = 0;
+        while (k <= i+1){
+            str = str + "|";
+            k++;
+        }
+    }
+    //do the end case seperately
 }
 
 int getUserInput()
 {
+    std:cout<<"requesting pyrimad size";
+    return 0;
+    //need to return the size
 
 }
